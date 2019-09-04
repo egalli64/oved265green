cdddecfb94e32a139238844289d9e3d0ed893ddc
Administrator <Administrator@192.168.12.53>
2019-09-04 15:29:46 +0200

    root.setup.sql launching instructions added 15.29 04/09/2019

--- green/model/root.setup.sql
+++ green/model/root.setup.sql
@@ -3,3 +3,13 @@ create user green identified by code2 account unlock;
 grant connect, resource to green;
 alter user green quota unlimited on users;
 exit
+
+-- 1)Entrare nella directory di questo file e aprire la shell di comando 
+--   (cmd sull'indirizzo della directory).
+
+-- 2)Entrare su sqlplus come sysdba copiando il codice alla riga 1
+
+-- 3)eseguire il seguente script con il comando "@root.setup.sql"
+
+
+-- CHEERS! 
