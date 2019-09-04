050cd84fe973d6d7496ec5dba0278b7140276126
Administrator <Administrator@192.168.12.53>
2019-09-04 17:38:32 +0200

    just few parameters into UserBean.java

--- green/src/main/java/users/UserBean.java
+++ green/src/main/java/users/UserBean.java
@@ -2,4 +2,10 @@ package users;
 
 public class UserBean {
 	
+	private Long userId;
+	private String userName;
+	private String userLastName;
+	private String phoneNumber;
+	private String mail;
+	
 }
