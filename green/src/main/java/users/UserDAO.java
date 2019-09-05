e1905d67f61d79b890a342051d38f63b96390e6e
Administrator <Administrator@192.168.12.53>
2019-09-05 14:57:50 +0200

    new method UserDAO.java

--- green/src/main/java/users/UserDAO.java
+++ green/src/main/java/users/UserDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import javax.sql.DataSource;
 
 public class UserDAO {
@@ -33,9 +35,47 @@ public class UserDAO {
 			} else {
 				return false;
 			}
-			
+
 		} catch (SQLException se) {
 			throw new IllegalStateException("Database issue " + se.getMessage());
-		}	
-			}
+		}
+	}
+
+	public boolean createNewUser(String userName, String userLastName, String phone, String email, String password,
+			LocalDate birthDate, String birthPlace, String license, LocalDate expiringLicense, Long creditCard,
+			Integer cvv, LocalDate expirationcard) {  
+	      
+		try {
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");  
+			PreparedStatement createNewUserQuery = null;
+			String s = "INSERT INTO users VALUES(user_seq.nextval, '?', '?', ?, '?','?','?', '?' , '?','?', ?, ?, '?')";
+			createNewUserQuery = conn.prepareStatement(s);
+			createNewUserQuery.setString(1, userName);
+			createNewUserQuery.setString(2, userLastName);
+			createNewUserQuery.setString(3, phone);
+			createNewUserQuery.setString(4, email);
+			createNewUserQuery.setString(5, password);
+			createNewUserQuery.setString(6, formatter.format(birthDate));
+			createNewUserQuery.setString(7, birthPlace);
+			createNewUserQuery.setString(8, license);
+			createNewUserQuery.setString(9, formatter.format(expiringLicense));
+			createNewUserQuery.setLong(10, creditCard);
+			createNewUserQuery.setInt(11, cvv);
+			createNewUserQuery.setString(12, formatter.format(expirationcard));			
+			createNewUserQuery.executeQuery();
+			return true;
+			
+//			UserBean user = new UserBean();			
+//			while (rs.next()) {
+//				user.setUserName(rs.getString("FIRST_NAME"));
+//				user.setUserName(rs.getString("FIRST_NAME"));
+//				user.setUserName(rs.getString("FIRST_NAME"));
+//				user.setUserName(rs.getString("FIRST_NAME"));
+//			}
+
+		} catch (SQLException se) {
+			throw new IllegalStateException("Database issue " + se.getMessage());
+		}
+	}
+
 }
