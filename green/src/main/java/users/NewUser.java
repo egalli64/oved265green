03f081ffe46168a94f80a490a9a663afa88e9998
Administrator <Administrator@192.168.12.53>
2019-09-05 15:00:37 +0200

    NewUser.java

--- green/src/main/java/users/NewUser.java
+++ green/src/main/java/users/NewUser.java
@@ -1,6 +1,7 @@
 package users;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -21,9 +22,21 @@ public class NewUser extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-//		CountryDAO dao = new CountryDAO(ds);
-//		String regionId = request.getParameter("regionId");
-//
+//		UserDAO userDao = new UserDAO(ds);
+//		String firstName = request.getParameter("firstName");
+//		String LastName = request.getParameter("LastName");
+//		String phone = request.getParameter("phone");
+//		String email = request.getParameter("email");
+//		LocalDate birthdate = request.getParameter("birthdate");
+//		String password = request.getParameter("password");
+//		String birthplace = request.getParameter("birthplace");
+//		String firstName = request.getParameter("firstName");
+//		String firstName = request.getParameter("firstName");
+//		String firstName = request.getParameter("firstName");
+//		String firstName = request.getParameter("firstName");
+//		String firstName = request.getParameter("firstName");
+		
+
 //		if (regionId != null) {
 //			request.setAttribute("countries", dao.getCountriesByRegion(regionId));
 //		} else {
