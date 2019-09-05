973dbefb109db3e60e09507b7d3608b1c605b458
Administrator <Administrator@192.168.12.52>
2019-09-05 10:46:22 +0200

    psw sql + foto audi html + psw html + login html

--- green/model/setup.sql
+++ green/model/setup.sql
@@ -9,6 +9,7 @@ first_name varchar2(40),
 last_name varchar2(40),
 phone_number varchar2(13), 
 email varchar2(100),
+password varchar2(20),
 data_nascita date, 
 luogo_nascita varchar2(40),
 n_patente varchar2(10),
@@ -21,9 +22,9 @@ scadenza_carta date
 CREATE SEQUENCE user_seq; 
 
 
-insert INTO users values (user_seq.nextval, 'Maurizio', 'Pilato', 3664464008, 'maurizio.pilato90@gmail.com','19-OTT-1990', 'Caltanissetta' , '800A','19-OTT-2023', 1254789632212471, 123, '19-OTT-2025');
-insert INTO users values (user_seq.nextval, 'Silvia', 'Di Caro', 3291909119, 'silviadicaro@libero.it','23-LUG-1993', 'Palermo' , '700A','23-LUG-2026', 1754896322212471, 458, '23-LUG-2028');
-insert INTO users values (user_seq.nextval, 'Federica', 'Cosenza', 3664464008, 'federica.cosenza92@gmail.com','23-LUG-1992', 'Messina' , '600A','23-LUG-2025', 1754896322754123, 215, '23-LUG-2027');
+insert INTO users values (user_seq.nextval, 'Maurizio', 'Pilato', 3664464008, 'maurizio.pilato90@gmail.com','mauro90','19-OTT-1990', 'Caltanissetta' , '800A','19-OTT-2023', 1254789632212471, 123, '19-OTT-2025');
+insert INTO users values (user_seq.nextval, 'Silvia', 'Di Caro', 3291909119, 'silviadicaro@libero.it','silvia93','23-LUG-1993', 'Palermo' , '700A','23-LUG-2026', 1754896322212471, 458, '23-LUG-2028');
+insert INTO users values (user_seq.nextval, 'Federica', 'Cosenza', 3664464008, 'federica.cosenza92@gmail.com','fede92','23-LUG-1992', 'Messina' , '600A','23-LUG-2025', 1754896322754123, 215, '23-LUG-2027');
 
 
 --Tabella Amministratori
