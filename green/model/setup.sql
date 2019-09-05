376f95870527c5eff5d34708ea1f3f7f25922420
Administrator <Administrator@192.168.12.52>
2019-09-05 09:35:24 +0200

    cvv+scadenza carta+ scadenza patente in sql e in html

--- green/model/setup.sql
+++ green/model/setup.sql
@@ -14,15 +14,16 @@ luogo_nascita varchar2(40),
 n_patente varchar2(10),
 scadenza_patente date,
 carta_credito number(16,0), 
-cvv number(3,0)
+cvv number(3,0),
+scadenza_carta date
 );
 
 CREATE SEQUENCE user_seq; 
 
 
-insert INTO users values (user_seq.nextval, 'Maurizio', 'Pilato', 3664464008, 'maurizio.pilato90@gmail.com','19-OTT-1990', 'Caltanissetta' , '800A','19-OTT-2023', 1254789632212471, 123);
-insert INTO users values (user_seq.nextval, 'Silvia', 'Di Caro', 3291909119, 'silviadicaro@libero.it','23-LUG-1993', 'Palermo' , '700A','23-LUG-2026' 1754896322212471, 458);
-insert INTO users values (user_seq.nextval, 'Federica', 'Cosenza', 3664464008, 'federica.cosenza92@gmail.com','23-LUG-1992', 'Messina' , '600A','23-LUG-2025' 1754896322754123, 215);
+insert INTO users values (user_seq.nextval, 'Maurizio', 'Pilato', 3664464008, 'maurizio.pilato90@gmail.com','19-OTT-1990', 'Caltanissetta' , '800A','19-OTT-2023', 1254789632212471, 123, '19-OTT-2025');
+insert INTO users values (user_seq.nextval, 'Silvia', 'Di Caro', 3291909119, 'silviadicaro@libero.it','23-LUG-1993', 'Palermo' , '700A','23-LUG-2026', 1754896322212471, 458, '23-LUG-2028');
+insert INTO users values (user_seq.nextval, 'Federica', 'Cosenza', 3664464008, 'federica.cosenza92@gmail.com','23-LUG-1992', 'Messina' , '600A','23-LUG-2025', 1754896322754123, 215, '23-LUG-2027');
 
 
 --Tabella Amministratori
