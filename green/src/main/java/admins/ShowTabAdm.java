d26e4d192071a03d7fee0cd9d6502daa0bd00fd9
Administrator <Administrator@192.168.12.53>
2019-09-06 15:28:36 +0200

    Added ShowTabAdmn.java servlet

--- green/src/main/java/admins/ShowTabAdm.java
+++ green/src/main/java/admins/ShowTabAdm.java
@@ -1,5 +1,34 @@
 package admins;
 
-public class ShowTabAdm {
+import java.io.IOException;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+@WebServlet("/admins/ShowTabUser")
+public class ShowTabAdm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Resource(name = "jdbc/green")
+	private DataSource ds;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		AdminDAO adminDao = new AdminDAO(ds);
+		request.setAttribute("admins", adminDao.showAdmins());
+		request.getRequestDispatcher("/adminTab.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
